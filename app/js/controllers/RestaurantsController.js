ef9976408f3d00a992dfb9348acab9a46ea82e7a
Volker Sommerfeld <vsommerfeld@gmail.com>
2013-02-10 11:36:12 +0100

    merged

--- app/js/controllers/RestaurantsController.js
+++ app/js/controllers/RestaurantsController.js
@@ -1,13 +1,5 @@
 'use strict';
 
-function getParameter(param) {
-            var val = document.URL;
-            var url = val.substr(val.indexOf(param))  
-            var n=url.replace(param+"=","");
-            //alert(n);
-            return n; 
-}
-
 foodMeApp.controller('RestaurantsController',
     function RestaurantsController($scope, customer, $location, $window, Restaurant, zxConnect, zxConnectSessionFacade) {
 
@@ -15,7 +7,6 @@ foodMeApp.controller('RestaurantsController',
   var zxConnectCredentials = zxConnect.getCredentials();
   if (zxConnectCredentials.connectId === undefined || zxConnectCredentials.secretKey === undefined) {
     console.log($location.absUrl());
-<<<<<<< HEAD
     //var authtoken = $location.search()['authtoken'];
     var authtoken = getParameter('authtoken');
     //console.log('authtoken: ', authtoken);
@@ -23,15 +14,6 @@ foodMeApp.controller('RestaurantsController',
     if (authtoken !== undefined) {
       // remove last 2 letters from authtoken (comes from angular: #/)
       authtoken = authtoken.substr(0, authtoken.length - 2);
-=======
-    //console.log($location.url());
-    var authtoken = getParameter('authtoken');
-    // remove #/:
-    authtoken = authtoken.substring(0, authtoken.length - 2);
-    console.log(authtoken.length);
-    //console.log($location.routeParams());
-    if (authtoken.length > 0) { // TODO: check for undefined, null etc.
->>>>>>> 628341dd24ea4970f34ff51608c87af1b39f1364
       console.log('authtoken: ', authtoken);
       
       // TODO: get zx connect session and set it to zxConnect service
@@ -43,15 +25,7 @@ foodMeApp.controller('RestaurantsController',
       // redirect to the zanox connect login page
       //console.log($location.absUrl());
       console.log('redirect to zx login page');
-<<<<<<< HEAD
       $window.location = 'https://auth.zanox.com/login?appid=D71378049E083896051C&callback=' + $location.absUrl();
-=======
-      //var absUrl = $location.absUrl();
-      //absUrl = absUrl.substring(0, absUrl.length - 2);
-      //console.log('https://auth.zanox.com/login?callback=' + absUrl);
-      
-      $window.location = 'https://auth.zanox.com/login?callback=' + $location.absUrl();
->>>>>>> 628341dd24ea4970f34ff51608c87af1b39f1364
       //$location.url('/customer');
     } 
   }
